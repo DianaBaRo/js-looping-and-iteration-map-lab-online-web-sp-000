0e76beab0e21dc391975c15aa0a627a5c93e571f
Diana Badas <diana.badas@gmail.com>
2019-07-28 14:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function lowerCaseDrivers(drivers) {
     return name.toLowerCase();
   });
 }
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(name) {
+    const first = name.split(' ')[0];
+    const last = name.split(' ')[1];
+
+    return { firstName: first, lastName: last };
+  });
+}
