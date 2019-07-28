cde3144c1514049e2a0affb4964a189e12fc5d16
Diana Badas <diana.badas@gmail.com>
2019-07-28 14:00:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
   return drivers.map(function(name){
-    name.toLowerCase();
-  })
+    return name.toLowerCase();
+  });
 }
